ce65d274672b34a5112296e65f0c74b491fdf9ef
Parthipakumar S <parthi@gamil.com>
2026-09-13 16:59:15 +0530

    calculate_the_area_of_rectangle

---
+++ Rectangle.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Rectangle{
+
+    public  static void main(String[] args){
+        double width=0;
+        double length=0;
+        double area=0;
+
+        Scanner cr = new Scanner(System.in);
+
+        System.out.print("ENTER THE WIDTH:");
+        width=cr.nextDouble();
+
+        System.out.print("ENTER THE LENGTH");
+        length=cr.nextDouble();
+
+        area=width*length;
+        System.out.println("AREA :"+area);
+    }
+}
