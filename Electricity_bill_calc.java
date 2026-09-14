7e21dd3abd23483745842c52cd7e67d228807f4c
Parthipakumar S <parthi@gamil.com>
2026-09-15 01:26:09 +0530

    electricity_bill_basic_calculator

---
+++ Electricity_bill_calc.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Electricity_bill_calc {
+    public static void main(String[] args){
+        int unit;
+        int rate;
+        int total_bill;
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.print("ENTER THE UNIT:");
+        unit=sc.nextInt();
+
+        System.out.print("ENTER THE ONE UNIT RATE");
+        rate=sc.nextInt();
+
+        total_bill=unit*rate;
+
+        System.out.println("\nUNITS:"+unit);
+        System.out.println("TOTAL BILL:"+total_bill);
+
+        sc.close();
+    }
+}
