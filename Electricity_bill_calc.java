fc5c930495a2e2205bf39c10770c69599aa7e074
Parthipakumar S <parthi@gamil.com>
2026-09-17 12:02:07 +0530

    method

--- Electricity_bill_calc.java
+++ Electricity_bill_calc.java
@@ -22,3 +22,4 @@ public class Electricity_bill_calc {
         sc.close();
     }
 }
+
---
+++ methods.java
@@ -0,0 +1,36 @@
+public class methods {
+    void  add(){ // Method
+        int a=10;
+        int b=5;
+        int c=a+b;
+        System.out.println("THE NUMBER IS ADD:"+c);
+    }
+
+    void sub(int a,int b){
+        int c=a-b;
+        System.out.println("THE NUMBER IS SUB:"+c);
+    }
+
+    void String(String a, String b){
+        System.out.println("THE STRING IS ADD"+a+b);
+    }
+
+    int return_type(int a,int b){
+        int c=a+b;
+        return c;
+    }
+
+    public static void main(String[] args){ // Main Method
+        
+        methods ms= new methods(); //Object Create
+
+        ms.add(); // Function Call
+        ms.sub(20,5);
+        ms.String("parthipa","kumar");
+
+        int d=ms.return_type(45,6); // Return Function Call
+        System.out.println("THE RETURN TYPE :"+d);
+    }   
+}
+
+
